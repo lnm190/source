a2dc683ec84542de3066bfed9c4bacfdd3693e0c
lnm190 <lnm190@users.noreply.github.com>
2017-07-21 14:14:37 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 $(function () {
     /*   全屏滚动插件 fullPage 初始化    */
     $('#fullPage').fullpage({
-        sectionsColor: ['#66cccc','#99cccc','#ccccff','#99cccc','#ccffcc'],
+        sectionsColor: ['#66cccc','#99cccc','#ccccff','#99cccc','#99ccff'],
         scrollingSpeed: 300,
         navigation: true,
         navigationPosition: 'right',
